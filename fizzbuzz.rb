761b0420d7c4d10bad427b5078fe9d8b51fdb1e4
Srishti Nath <srishti.nath@gmail.com>
2020-03-31 04:37:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,16 +2,16 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(integer)
   if (integer % 3 == 0)
-    puts "Fizz"
+    "Fizz"
 
   elsif (integer % 5 == 0)
-    puts "Buzz"
+    "Buzz"
 
   elsif (integer % 3 == 0 && integer % 5 == 0)
-    puts "FizzBuzz"
+    "FizzBuzz"
 
   else
-    puts ""
+    ""
   end
 
 end
