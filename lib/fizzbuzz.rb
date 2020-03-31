e12f150bc9c14c0c0a17b9f2555dc10cbd0c8f7f
Srishti Nath <srishti.nath@gmail.com>
2020-03-31 04:05:40 +0000

    Automatically backed up by Learn

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,12 @@
+# Don't forget! This file needs to be 'required' in its spec file
+# See README.md for instructions on how to do this
+def fizzbuzz
+  if int % 3 == 0
+    puts "Fizz"
+  elseif int % 5 == 0
+    puts "Buzz"
+  elseif int % 3 == 0 && int % 5 == 0
+    puts "FizzBuzz"
+  else
+    puts ""
+end
