44886b4ef00a32b2d65d8e765805626db5911a8c
Srishti Nath <srishti.nath@gmail.com>
2020-03-31 04:30:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(int)
   if int % 3 == 0
     puts "Fizz"
-  elseif int % 5 == 0
+  elsif int % 5 == 0
     puts "Buzz"
-  elseif int % 3 == 0 && int % 5 == 0
+  elsif int % 3 == 0 && int % 5 == 0
     puts "FizzBuzz"
   else
     puts ""
